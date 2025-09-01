7499801c1b8f8173978367877175068b5e091e2a
edp1096 <edp1096@naver.com>
2025-09-02 05:58:59 +0900

    Almost done.

--- src/main.lua
+++ src/main.lua
@@ -20,6 +20,7 @@ local text = require("text")
 
 local sound = nil
 local manual_stop = false
+-- local music_stopping = false
 
 local default_volume = 0.1
 local current_volume = default_volume
@@ -55,43 +56,59 @@ local boss_bgm_names = {
     -- Abaddon_Finish = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_Abaddon_FINISH.BGM_DED_BOSS_Abaddon_FINISH",
     -- Corrupter = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_Grubshooter.BGM_DED_BOSS_Grubshooter", -- There is no Corrupter_Finish
     Corrupter = "BGM/F02/BGM_DED_BOSS_Grubshooter",
-    Gigas = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_Gigas_Cue.BGM_DED_BOSS_Gigas_Cue",
-    Gigas_Finish = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_GIGAS_FINISH.BGM_DED_BOSS_GIGAS_FINISH",
-    Gigas_ChallengeDone = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_GIGAS_FINISH.BGM_DED_BOSS_GIGAS_FINISH",
-    Brute = "/Game/Sound/World/BGM/E03/BGM_WASTELAND_BOSS_BRUTE_P1.BGM_WASTELAND_BOSS_BRUTE_P1",
-    Brute_P2 = "/Game/Sound/World/BGM/E03/BGM_WASTELAND_BOSS_BRUTE_P2.BGM_WASTELAND_BOSS_BRUTE_P2",
-    Gigas_Wasteland = "/Game/Sound/World/BGM/E03/BGM_WASTELAND_BOSS_GIGAS_Cue.BGM_WASTELAND_BOSS_GIGAS_Cue",
-    Gigas_Wasteland_Finish = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_GIGAS_FINISH.BGM_DED_BOSS_GIGAS_FINISH",
-    Stalker = "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_Sawshark_Cue.BGM_ME_BOSS_Sawshark_Cue",
-    Stalker_Finish = "/Game/Sound/World/BGM/BGMEndSound/BGM_ENDSound_STALKER.BGM_ENDSound_STALKER",
-    Juggernaut = "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_JUGGERNAUT_BELIAL_P1.BGM_ME_BOSS_JUGGERNAUT_BELIAL_P1",
-    Juggernaut_Finish = "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_JUGGERNAUT_FINISH.BGM_ME_BOSS_JUGGERNAUT_FINISH",
-    Tachy = "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_Tachy_P1_Cue.BGM_ME_BOSS_Tachy_P1_Cue",
-    Tachy_Finish = "/Game/Sound/World/BGM/Matrix_XI/BGM_BOSS_TACHY_FINISH.BGM_BOSS_TACHY_FINISH",
-    Stalker_GreatDesert =
-    "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_STALKER_P1_LOOP_170.BGM_ME_BOSS_STALKER_P1_LOOP_170",
-    Stalker_GreatDesert_Finish = "/Game/Sound/World/BGM/E05/BGM_E05_BOSS_Sawshark_P2_Cue", -- Actually Phase 2
+    Gigas = "BGM/F02/BGM_DED_BOSS_Gigas",
+    -- Gigas = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_Gigas_Cue.BGM_DED_BOSS_Gigas_Cue",
+    -- Gigas_Finish = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_GIGAS_FINISH.BGM_DED_BOSS_GIGAS_FINISH",
+    -- Gigas_ChallengeDone = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_GIGAS_FINISH.BGM_DED_BOSS_GIGAS_FINISH",
+    -- Brute = "/Game/Sound/World/BGM/E03/BGM_WASTELAND_BOSS_BRUTE_P1.BGM_WASTELAND_BOSS_BRUTE_P1",
+    -- Brute_P2 = "/Game/Sound/World/BGM/E03/BGM_WASTELAND_BOSS_BRUTE_P2.BGM_WASTELAND_BOSS_BRUTE_P2",
+    Brute = "BGM/E03/BGM_WASTELAND_BOSS_BRUTE",
+    -- Gigas_Wasteland = "/Game/Sound/World/BGM/E03/BGM_WASTELAND_BOSS_GIGAS_Cue.BGM_WASTELAND_BOSS_GIGAS_Cue",
+    -- Gigas_Wasteland_Finish = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_GIGAS_FINISH.BGM_DED_BOSS_GIGAS_FINISH",
+    Gigas_Wasteland = "BGM/E03/BGM_WASTELAND_BOSS_GIGAS",
+    -- Stalker = "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_Sawshark_Cue.BGM_ME_BOSS_Sawshark_Cue",
+    -- Stalker_Finish = "/Game/Sound/World/BGM/BGMEndSound/BGM_ENDSound_STALKER.BGM_ENDSound_STALKER",
+    Stalker = "BGM/Matrix_XI/BGM_ME_BOSS_Sawshark",
+    -- Juggernaut = "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_JUGGERNAUT_BELIAL_P1.BGM_ME_BOSS_JUGGERNAUT_BELIAL_P1",
+    -- Juggernaut_Finish = "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_JUGGERNAUT_FINISH.BGM_ME_BOSS_JUGGERNAUT_FINISH",
+    Juggernaut = "BGM/Matrix_XI/BGM_ME_BOSS_JUGGERNAUT",
+    -- Tachy = "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_Tachy_P1_Cue.BGM_ME_BOSS_Tachy_P1_Cue",
+    -- Tachy_Finish = "/Game/Sound/World/BGM/Matrix_XI/BGM_BOSS_TACHY_FINISH.BGM_BOSS_TACHY_FINISH",
+    Tachy = "BGM/Matrix_XI/BGM_ME_BOSS_Tachy",
+    -- Stalker_GreatDesert =
+    -- "/Game/Sound/World/BGM/Matrix_XI/BGM_ME_BOSS_STALKER_P1_LOOP_170.BGM_ME_BOSS_STALKER_P1_LOOP_170",
+    -- Stalker_GreatDesert_Finish = "/Game/Sound/World/BGM/E05/BGM_E05_BOSS_Sawshark_P2_Cue", -- Actually Phase 2
+    Stalker_GreatDesert = "BGM/Matrix_XI/BGM_ME_BOSS_STALKER",
     -- Abaddon_GreatDesert = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_Abaddon.BGM_DED_BOSS_Abaddon", -- Same as Abaddon
-    Behemoth = "/Game/Sound/World/BGM/E05/BGM_E03_BOSS_Behemoth.BGM_E03_BOSS_Behemoth",
-    Behemoth_Finish = "/Game/Sound/World/BGM/BGMEndSound/BGM_ENDSound_BEHEMOTH.BGM_ENDSound_BEHEMOTH",
+    -- Behemoth = "/Game/Sound/World/BGM/E05/BGM_E03_BOSS_Behemoth.BGM_E03_BOSS_Behemoth",
+    -- Behemoth_Finish = "/Game/Sound/World/BGM/BGMEndSound/BGM_ENDSound_BEHEMOTH.BGM_ENDSound_BEHEMOTH",
+    Behemoth = "BGM/E05/BGM_E03_BOSS_Behemoth",
     -- Corrupter_Eidos9 = "/Game/Sound/World/BGM/F02/BGM_DED_BOSS_Grubshooter.BGM_DED_BOSS_Grubshooter", -- Same as Corrupter
     -- Belial = "/Game/Sound/World/BGM/B07/BossBattle/BGM_SE_BOSS_Belial_Cue.BGM_SE_BOSS_Belial_Cue", -- There's no BGMEndSound for Belial
-    Karakuri = "/Game/Sound/World/BGM/B07/BossBattle/BGM_SE_BOSS_Karakuri_Cue.BGM_SE_BOSS_Karakuri_Cue",
-    Karakuri_Finish = "/Game/Sound/World/BGM/BGMEndSound/BGM_ENDSound_Karakuri.BGM_ENDSound_Karakuri",
-    Democrawler = "/Game/Sound/World/BGM/B07/BossBattle/BGM_SE_BOSS_Crawler_P1_Cue.BGM_SE_BOSS_Crawler_P1_Cue",
-    Democrawler_Finish = "/Game/Sound/World/BGM/BGMEndSound/EVE_SE_BGMEndSound_2.EVE_SE_BGMEndSound_2", -- Actually DemoGorgon
-    RavenBeast = "/Game/Sound/World/BGM/E04/BGM_XION_BOSS_RavenBeast_P1_Cue.BGM_XION_BOSS_RavenBeast_P1_Cue",
-    RavenBeast_Finish = "/Game/Sound/World/BGM/E04/BGM_XION_BOSS_RAVENBEAST_FINISH.BGM_XION_BOSS_RAVENBEAST_FINISH",
-    Raven = "/Game/Sound/World/BGM/E03/BGM_NEST_BOSS_RAVEN_P1_Cue.BGM_NEST_BOSS_RAVEN_P1_Cue",
-    Raven_Finish = "/Game/Sound/World/BGM/E03/BGM_NEST_BOSS_RAVEN_FINISH.BGM_NEST_BOSS_RAVEN_FINISH",
-    Providence = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_LILY_P1_CUE.BGM_NEST_BOSS_LILY_P1_CUE",
-    Providence_Lily = "/Game/Sound/World/BGM/Nest/BGM_NEST_LILY_P3_SAVE_BATTLE_143.BGM_NEST_LILY_P3_SAVE_BATTLE_143",
-    Providence_Li_ly = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_LILY_P3_DIE.BGM_NEST_BOSS_LILY_P3_DIE",
-    Elder = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_ELDER_P2_CUE.BGM_NEST_BOSS_ELDER_P2_CUE",
-    Elder_01_Finish = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_ELDER_ENDING_01.BGM_NEST_BOSS_ELDER_ENDING_01",
-    Elder_02_Finish = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_ELDER_ENDING_02.BGM_NEST_BOSS_ELDER_ENDING_02",
-    Mann = "/Game/Sound/World/BGM/Nikke/BGM_D2_BOSS_MANN_LOOP.BGM_D2_BOSS_MANN_LOOP",
-    Scarlet = "/Game/Sound/World/BGM/Nikke/BGM_BOSS_SCARLET_P1_Cue.BGM_BOSS_SCARLET_P1_Cue",
+    -- Karakuri = "/Game/Sound/World/BGM/B07/BossBattle/BGM_SE_BOSS_Karakuri_Cue.BGM_SE_BOSS_Karakuri_Cue",
+    -- Karakuri_Finish = "/Game/Sound/World/BGM/BGMEndSound/BGM_ENDSound_Karakuri.BGM_ENDSound_Karakuri",
+    Karakuri = "BGM/B07/BossBattle/BGM_SE_BOSS_Karakuri",
+    -- Democrawler = "/Game/Sound/World/BGM/B07/BossBattle/BGM_SE_BOSS_Crawler_P1_Cue.BGM_SE_BOSS_Crawler_P1_Cue",
+    -- Democrawler_Finish = "/Game/Sound/World/BGM/BGMEndSound/EVE_SE_BGMEndSound_2.EVE_SE_BGMEndSound_2", -- Actually DemoGorgon
+    Democrawler = "BGM/B07/BossBattle/BGM_SE_BOSS_Crawler",
+    -- RavenBeast = "/Game/Sound/World/BGM/E04/BGM_XION_BOSS_RavenBeast_P1_Cue.BGM_XION_BOSS_RavenBeast_P1_Cue",
+    -- RavenBeast_Finish = "/Game/Sound/World/BGM/E04/BGM_XION_BOSS_RAVENBEAST_FINISH.BGM_XION_BOSS_RAVENBEAST_FINISH",
+    RavenBeast = "BGM/E04/BGM_XION_BOSS_RavenBeast",
+    -- Raven = "/Game/Sound/World/BGM/E03/BGM_NEST_BOSS_RAVEN_P1_Cue.BGM_NEST_BOSS_RAVEN_P1_Cue",
+    -- Raven_Finish = "/Game/Sound/World/BGM/E03/BGM_NEST_BOSS_RAVEN_FINISH.BGM_NEST_BOSS_RAVEN_FINISH",
+    Raven = "BGM/E03/BGM_NEST_BOSS_RAVEN",
+    -- Providence = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_LILY_P1_CUE.BGM_NEST_BOSS_LILY_P1_CUE",
+    -- Providence_Lily = "/Game/Sound/World/BGM/Nest/BGM_NEST_LILY_P3_SAVE_BATTLE_143.BGM_NEST_LILY_P3_SAVE_BATTLE_143",
+    -- Providence_Li_ly = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_LILY_P3_DIE.BGM_NEST_BOSS_LILY_P3_DIE",
+    Providence = "BGM/Nest/BGM_NEST_BOSS_LILY",
+    -- Elder = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_ELDER_P2_CUE.BGM_NEST_BOSS_ELDER_P2_CUE",
+    -- Elder_01_Finish = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_ELDER_ENDING_01.BGM_NEST_BOSS_ELDER_ENDING_01",
+    -- Elder_02_Finish = "/Game/Sound/World/BGM/Nest/BGM_NEST_BOSS_ELDER_ENDING_02.BGM_NEST_BOSS_ELDER_ENDING_02",
+    Elder = "BGM/Nest/BGM_NEST_BOSS_ELDER",
+    -- Mann = "/Game/Sound/World/BGM/Nikke/BGM_D2_BOSS_MANN_LOOP.BGM_D2_BOSS_MANN_LOOP",
+    -- Scarlet = "/Game/Sound/World/BGM/Nikke/BGM_BOSS_SCARLET_P1_Cue.BGM_BOSS_SCARLET_P1_Cue",
+    Mann = "BGM/Nikke/BGM_D2_BOSS_MANN",
+    Scarlet = "BGM/Nikke/BGM_BOSS_SCARLET"
 }
 
 
@@ -151,7 +168,8 @@ local function playMusic(music_file)
 end
 
 local function stopMusic()
-    if not sound then return false end
+    -- if not sound or not music_stopping then return false end
+    -- music_stopping = true
 
     local fadeout_volume = current_volume
     local volume_down_step = current_volume / 50
@@ -165,7 +183,9 @@ local function stopMusic()
     end
 
     sound:stop()
-    sound = nil
+    -- sound = nil
+
+    -- music_stopping = false
 
     -- dprint("Music is stopped." .. current_music_files[current_music_index])
     dprint("Music is stopped.")
@@ -209,6 +229,7 @@ local function togglePlay()
     if sound and sound:isPlaying() then
         dprint("Stop music")
         manual_stop = true
+        -- while music_stopping do audio.msleep(100) end
         ExecuteAsync(function() stopMusic() end)
     else
         dprint("Play music")
@@ -298,7 +319,7 @@ local function controlBossBGM(ctx)
                 -- Single SoundWave
                 for k, boss_bgm_name in pairs(boss_bgm_names) do
                     if string.find(audio_component["wave_name"], boss_bgm_name) then
-                        audio_component["boss_name_key"] = k
+                        audio_component["boss_name_key"] = text:Split(k, "_")[1]
                         break
                     end
                 end
@@ -306,7 +327,7 @@ local function controlBossBGM(ctx)
                 -- Multiple SoundWaves or VendingMachine or SoundNodeSwitch or Mixer or others
                 for k, boss_bgm_name in pairs(boss_bgm_names) do
                     if string.find(audio_component["cue_name"], boss_bgm_name) then
-                        audio_component["boss_name_key"] = k
+                        audio_component["boss_name_key"] = text:Split(k, "_")[1]
                         break
                     end
                 end
@@ -328,7 +349,7 @@ local function controlBossBGM(ctx)
 
                 if not is_boss_bgm_triggered then
                     local fname = text:TrimSpace(music_files_boss[audio_component["boss_name"]])
-                    dprint("Current music fname: " .. fname)
+                    dprint("Current music fname: " .. tostring(fname))
                     if fname ~= "" then
                         current_music_files = { music_dirs["Boss"] .. "/" .. fname }
 
@@ -348,7 +369,7 @@ local function controlBossBGM(ctx)
 
                     ExecuteAsync(function()
                         manual_stop = true
-                        audio.msleep(500)
+                        -- while music_stopping do audio.msleep(100) end
                         if sound then stopMusic() end
                         playShuffleBoss(current_music_files)
                         manual_stop = false
@@ -396,7 +417,7 @@ local function controlBossBGM(ctx)
 
             ExecuteAsync(function()
                 manual_stop = true
-                audio.msleep(500)
+                -- while music_stopping do audio.msleep(100) end
                 if sound then stopMusic() end
                 playShuffle(current_music_files)
                 manual_stop = false
@@ -454,6 +475,7 @@ ExecuteWithDelay(5000, function()
         ExecuteAsync(function()
             manual_stop = true
             audio.msleep(stage_time_append)
+            -- while music_stopping do audio.msleep(100) end
             if sound then stopMusic() end
             playShuffle(current_music_files)
             manual_stop = false
--- src/text.lua
+++ src/text.lua
@@ -1,7 +1,99 @@
 local text = {}
 
+-- Trim whitespace from both ends of string
 function text:TrimSpace(s)
+    -- if not s then return nil end
     return s:match("^%s*(.-)%s*$")
 end
+
+-- Split string by separator
+function text:Split(str, sep)
+    if not str then return {} end
+    if not sep then sep = "%s" end -- Default to whitespace
+
+    local result = {}
+    local pattern
+
+    -- Special handling for whitespace separators
+    if sep == " " or sep == "%s" then
+        -- Handle multiple consecutive spaces as single separator
+        pattern = "([^%s]+)"
+        for part in string.gmatch(str, pattern) do
+            table.insert(result, part)
+        end
+    else
+        -- Handle other separators (escape special pattern characters)
+        local escaped_sep = sep:gsub("([%.%+%-%*%?%[%]%(%)%^%$%%])", "%%%1")
+        pattern = "([^" .. escaped_sep .. "]+)"
+        for part in string.gmatch(str, pattern) do
+            table.insert(result, part)
+        end
+    end
+
+    return result
+end
+
+-- -- Alternative: Split with exact whitespace handling
+-- function text:SplitExact(str, sep)
+--     if not str then return {} end
+--     if not sep then sep = " " end -- Single space default
+
+--     local result = {}
+--     local start = 1
+--     local sep_len = #sep
+
+--     while true do
+--         local pos = string.find(str, sep, start, true) -- Plain text search
+--         if pos then
+--             local part = string.sub(str, start, pos - 1)
+--             table.insert(result, part) -- Include empty strings
+--             start = pos + sep_len
+--         else
+--             local part = string.sub(str, start)
+--             table.insert(result, part)
+--             break
+--         end
+--     end
+
+--     return result
+-- end
+
+-- Get first part before separator (your original use case)
+function text:GetFirstPart(str, sep)
+    if not str then return nil end
+    if not sep then sep = "_" end
+
+    local parts = self:Split(str, sep)
+    return parts[1]
+end
+
+-- Additional utility: Join array with separator
+function text:Join(array, sep)
+    if not array then return "" end
+    if not sep then sep = " " end
+    return table.concat(array, sep)
+end
+
+-- -- Test the module
+-- local test_string = "Gigas_Wasteland_More_Parts"
+-- print("Original:", test_string)
+-- print("Split by underscore:", table.concat(text:Split(test_string, "_"), ", "))
+-- print("First part:", text:GetFirstPart(test_string, "_"))
+
+-- -- Test with spaces
+-- local space_test = "Hello   World    Lua   Code"
+-- print("\nSpace tests:")
+-- print("Original:", "'" .. space_test .. "'")
+-- print("Split by space (auto-collapse):", table.concat(text:Split(space_test, " "), ", "))
+-- print("Split exact spaces:", table.concat(text:SplitExact(space_test, " "), ", "))
+
+-- -- More space examples
+-- local simple_space = "Apple Banana Cherry"
+-- print("\nSimple space example:")
+-- print("Original:", simple_space)
+-- print("Split result:", table.concat(text:Split(simple_space, " "), ", "))
+-- print("First part:", text:GetFirstPart(simple_space, " "))
+
+-- print("Trimmed:", "'" .. text:TrimSpace("  spaced text  ") .. "'")
 
 return text
